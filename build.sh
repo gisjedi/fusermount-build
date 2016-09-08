a5f1ba1ed32ec5dad63b3f8da50971fc31d4bb69
Jonathan Meyer <jon@gisjedi.com>
2016-09-08 18:08:33 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 VERSION=$1
 
 cd /tmp
-yum install -y gcc make
 curl -L https://github.com/libfuse/libfuse/releases/download/fuse-$VERSION/fuse-$VERSION.tar.gz | tar xz
 cd fuse-$VERSION
 ./configure
